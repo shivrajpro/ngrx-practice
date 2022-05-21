c4fcd1d88aa717d597c23fa800bcba9dcc7925bc
shivrajpro <shivrajhulbatte@gmail.com>
2022-05-21 13:32:13 +0530

    add login success in reducer

--- src/app/auth/state/auth.reducer.ts
+++ src/app/auth/state/auth.reducer.ts
@@ -1,7 +1,13 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { loginSuccess } from "./auth.actions";
 import { initialState } from "./auth.state";
 
-const _authReducer = createReducer(initialState);
+const _authReducer = createReducer(initialState, on(loginSuccess, (state, action)=>{
+    return {
+        ...state,
+        user:action.user
+    }
+}));
 
 export function authReducer(state, action){
     return _authReducer(state, action);
---
+++ src/app/models/AuthResponseData.model.ts
@@ -0,0 +1,8 @@
+export interface AuthResponseData{
+    email: string;
+    expiresIn: string;
+    idToken: string;
+    localId: string;
+    refreshToken: string;
+    registered: boolean;    
+}
